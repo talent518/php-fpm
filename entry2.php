cbd8cec6336023176690283291fc24dd3aa02944
阿宝 <talent518@yeah.net>
2020-09-24 08:25:36 +0000

    demo 2 and README.md

--- entry2.php
+++ entry2.php
@@ -11,10 +11,19 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 function yii_application_run() {
+	static $time;
 	$config = require __DIR__ . '/../config/web.php';
-	(new yii\web\Application($config))->run();
-	rev_close(\Yii::$app);
-	\Yii::$app = null;
+	if(\Yii::$app !== null) {
+		\Yii::$app->set('request', \Yii::$app->components['request'] ?? ['class'=>'yii\web\Request']);
+		if($time + 600 < time()) {
+			syslog(LOG_DEBUG, "TIME");
+			rev_close(\Yii::$app);
+		}
+		\Yii::$app->run();
+	} else {
+		(new yii\web\Application($config))->run();
+	}
+	$time = time();
 }
 
 function rev_close(object $obj) {
