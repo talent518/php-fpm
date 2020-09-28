845117ba38602f7c9ee7f4a68730fb66b07a3fa5
阿宝 <talent518@yeah.net>
2020-09-28 05:13:54 +0000

    Update entry.php

--- entry.php
+++ entry.php
@@ -8,6 +8,8 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 function yii_application_run() {
+	undefine('YII_BEGIN_TIME');
+	define('YII_BEGIN_TIME', microtime(true));
 	$config = require __DIR__ . '/../config/web.php';
 	(new yii\web\Application($config))->run();
 	rev_close(\Yii::$app);
