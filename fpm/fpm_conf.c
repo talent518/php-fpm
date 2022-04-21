a3044911ea66ad4a7aa3d9e57cd7e5f98158d06e
阿宝 <talent518@yeah.net>
2022-04-21 22:08:45 +0800

    Modify sapi/fpm to sapi/fpm2, program to php-fpm2 ...

--- fpm/fpm_conf.c
+++ fpm/fpm_conf.c
@@ -1778,9 +1778,9 @@ int fpm_conf_init_main(int test_conf, int force_daemon) /* {{{ */
 		char *tmp;
 
 		if (fpm_globals.prefix == NULL) {
-			spprintf(&tmp, 0, "%s/php-fpm.conf", PHP_SYSCONFDIR);
+			spprintf(&tmp, 0, "%s/php-fpm2.conf", PHP_SYSCONFDIR);
 		} else {
-			spprintf(&tmp, 0, "%s/etc/php-fpm.conf", fpm_globals.prefix);
+			spprintf(&tmp, 0, "%s/etc/php-fpm2.conf", fpm_globals.prefix);
 		}
 
 		if (!tmp) {
